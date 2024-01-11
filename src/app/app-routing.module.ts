21307f0588c5fe7c5f3d912a5ab403411b8829ac
daliahussein <daliahussein999@gmail.com>
2024-01-11 11:09:55 +0200

    routing and data service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesListComponent } from './components/courses/courses-list/courses-list.component';
+import { CoursedetailsComponent } from './components/courses/coursedetails/coursedetails.component';
+import { WishlistComponent } from './components/courses/wishlist/wishlist.component';
+import { NotfoundPageComponent } from './components/courses/notfound-page/notfound-page.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { CartComponent } from './components/cart/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "" , component : CoursesListComponent},
+  {path : "home" , component : CoursesListComponent},
+  {path : "details" , component : CoursedetailsComponent},
+  {path : "wishlist", component : WishlistComponent},
+  {path : "login" , component : LoginComponent},
+  {path : "register" , component : RegisterComponent},
+  {path : "cart" , component : CartComponent},
+  {path : "**", component : NotfoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
@@ -9,6 +12,16 @@ import { CoursesListComponent } from './components/courses/courses-list/courses-
 import { CoursecardComponent } from './components/courses/coursecard/coursecard.component';
 import { CoursedetailsComponent } from './components/courses/coursedetails/coursedetails.component';
 import { WishlistComponent } from './components/courses/wishlist/wishlist.component';
+
+import { ButtonModule } from 'primeng/button';
+import { NotfoundPageComponent } from './components/courses/notfound-page/notfound-page.component';
+import { CartComponent } from './components/cart/cart/cart.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+
+
+
+
 
 
 @NgModule({
@@ -19,11 +32,19 @@ import { WishlistComponent } from './components/courses/wishlist/wishlist.compon
     CoursecardComponent,
     CoursedetailsComponent,
     WishlistComponent,
+    NotfoundPageComponent,
+    CartComponent,
+    LoginComponent,
+    RegisterComponent,
+    
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
